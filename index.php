0b0d1176180d494470165643ae026f5af9c37f4d
Vincen Wen <vincen.wen@samesamechina.com>
2015-03-18 14:46:33 +0800

    no message

--- index.php
+++ index.php
@@ -1,24 +1,23 @@
 <?php
-
-include_once('./config/database.php');
-include_once('./config/Pdb.php');
-include_once('./config/uUid.php');
-$db = Pdb::getDb();
-$s = isset($_GET['s']) ? $_GET['s'] : "";
-$id = substr($s,4);
-if(isset($_COOKIE['lv_ibeacon_uuid'])){
-    $uuid = $_COOKIE['lv_ibeacon_uuid'];
-}else{
-    $uuid = getUuid::guid();
-    @setcookie("lv_ibeacon_uuid", $uuid, time()+3600*24*365, "/");
-}
-$sql = "select id from lv_ibeacon_info where uuid=" . $db->quote($uuid);
-$rs = $db->getOne($sql);
-if($rs){
-    $finish=1;
-}else{
-    $finish=0;
-}
+// include_once('./config/database.php');
+// include_once('./config/Pdb.php');
+// include_once('./config/uUid.php');
+// $db = Pdb::getDb();
+// $s = isset($_GET['s']) ? $_GET['s'] : "";
+// $id = substr($s,4);
+// if(isset($_COOKIE['lv_ibeacon_uuid'])){
+//     $uuid = $_COOKIE['lv_ibeacon_uuid'];
+// }else{
+//     $uuid = getUuid::guid();
+//     @setcookie("lv_ibeacon_uuid", $uuid, time()+3600*24*365, "/");
+// }
+// $sql = "select id from lv_ibeacon_info where uuid=" . $db->quote($uuid);
+// $rs = $db->getOne($sql);
+// if($rs){
+//     $finish=1;
+// }else{
+//     $finish=0;
+// }
 
 // $id=1;$finish=0;
 ?> 
@@ -222,7 +221,7 @@ $menu.tap(function(event){
 </script>
 <script type="text/javascript">
   //main.js
-var objScript = new Object({
+var objScript = {
        isPhoneNum : function(value){
             return /^0?(13[0-9]|15[012356789]|18[012356789]|14[57])[0-9]{8}$/.test(value);
        },
@@ -266,7 +265,7 @@ var objScript = new Object({
               }
           })
        }
-});
+};
 
 </script>
 <script type="text/javascript">
