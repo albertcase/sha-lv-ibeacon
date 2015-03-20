20ae164f4b640beee4a005c7fb21f503e7973f11
Vincen Wen <vincen.wen@samesamechina.com>
2015-03-20 12:16:40 +0800

    no message

--- index.php
+++ index.php
@@ -1,24 +1,24 @@
 <?php
-// include_once('./config/database.php');
-// include_once('./config/Pdb.php');
-// include_once('./config/uUid.php');
-// $db = Pdb::getDb();
-// $s = isset($_GET['s']) ? $_GET['s'] : "";
-// $id = substr($s,4);
-// if(isset($_COOKIE['lv_ibeacon_uuid'])){
-//     $uuid = $_COOKIE['lv_ibeacon_uuid'];
-// }else{
-//     $uuid = getUuid::guid();
-//     @setcookie("lv_ibeacon_uuid", $uuid, time()+3600*24*365, "/");
-// }
-// if($id){
-//   $db->execute("insert into lv_ibeacon_pv set uuid=".$db->quote($uuid).",resource=".$db->quote($id));
-// }
-// $sql = "select id from lv_ibeacon_info where uuid=" . $db->quote($uuid);
-// $rs = $db->getOne($sql);
-// $finish = $rs ? 1 : 0;
+include_once('./config/database.php');
+include_once('./config/Pdb.php');
+include_once('./config/uUid.php');
+$db = Pdb::getDb();
+$s = isset($_GET['s']) ? $_GET['s'] : "";
+$id = substr($s,4);
+if(isset($_COOKIE['lv_ibeacon_uuid'])){
+    $uuid = $_COOKIE['lv_ibeacon_uuid'];
+}else{
+    $uuid = getUuid::guid();
+    @setcookie("lv_ibeacon_uuid", $uuid, time()+3600*24*365, "/");
+}
+if($id){
+  $db->execute("insert into lv_ibeacon_pv set uuid=".$db->quote($uuid).",resource=".$db->quote($id));
+}
+$sql = "select id from lv_ibeacon_info where uuid=" . $db->quote($uuid);
+$rs = $db->getOne($sql);
+$finish = $rs ? 1 : 0;
 
- $id=1;$finish=0;
+//$id=1;$finish=0;
 ?> 
 <!doctype html>
 <html>
